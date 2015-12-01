4309b7c99da87ac029cbbc164ee17b1f5b95a7d8
Hagan McPhail <hagan.lfd36@gmail.com>
2015-12-01 14:23:23 -0600

    initial commit

--- config/environment.js
+++ config/environment.js
@@ -2,6 +2,15 @@
 
 module.exports = function(environment) {
   var ENV = {
+    contentSecurityPolicy: {
+       'default-src': "'none'",
+        'script-src': "'self' 'unsafe-inline' 'unsafe-eval' use.typekit.net connect.facebook.net maps.googleapis.com maps.gstatic.com",
+        'font-src': "'self' data: use.typekit.net",
+        'connect-src': "'self'",
+        'img-src': "'self' www.facebook.com p.typekit.net",
+        'style-src': "'self' 'unsafe-inline' use.typekit.net",
+        'frame-src': "s-static.ak.facebook.com static.ak.facebook.com www.facebook.com"
+    },
     modulePrefix: 'library-app',
     environment: environment,
     baseURL: '/',
@@ -21,10 +30,10 @@ module.exports = function(environment) {
 
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.APP.LOG_ACTIVE_GENERATION = true;
+    ENV.APP.LOG_TRANSITIONS = true;
+    ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
   if (environment === 'test') {
